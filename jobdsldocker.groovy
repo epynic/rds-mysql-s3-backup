a84df55ea531806af22261f8798a9270fe3aa371
epynic <epynic@gmail.com>
2020-01-12 13:54:40 +0000

    added Jenkins job dsl build options

---
+++ jobdsldocker.groovy
@@ -0,0 +1,21 @@
+job('test-ops-s3-rds-backup'){
+    scm{
+        git('git://github.com/epynic/rds-mysql-s3-backup.git') { node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('me@me.com')
+        }
+    }
+    triggers{
+        scm('H/5 * * * *')
+    }
+    steps{
+        dockerBuildAndPublish {
+            repositoryName('epynic/rds-mysql-s3-backup')
+            tag('${GIT_REVISION,length=7}')
+            registryCredentials('DockerHub')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
